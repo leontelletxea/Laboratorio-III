2e256e64d47ed2274aaae4e846310ddad9e2259b
Tellechea Leon <60677124+leontelletxea@users.noreply.github.com>
2021-06-13 20:22:02 -0300

    Add files via upload

---
+++ Clase09/ejercicios/ejercicios.js
@@ -0,0 +1,42 @@
+"use strict";
+/// <reference path="MOCK_DATA.ts" />
+var users = datos.MOCK_DATA;
+//#1.-Mostrar todos los datos de los usuarios por consola
+//#2.-Retornar todos los trabajos de los usuarios
+var trabajos = function () { };
+//console.log(trabajos);
+//#3.-Retornar todos los paises de los usuarios
+var paises = function () { };
+//console.log(paises);
+//#4.-Retornar un array de objetos de aquellos usuarios cuyo pais sea China
+var chinas = function () { };
+//console.log(chinas);
+//#5.-Retornar una array de objetos de todos los usuarios menores a 21 años
+var jovenes = function () { };
+//console.log(jovenes);
+//#6.-Retornar la cantidad de usuarios con sexo masculino (Male)
+var cantidadMasculinos = function () { };
+//console.log(cantidadMasculinos);
+//#7.-Retornar una array de strings (el nombre de los usarios de sexo femenino (Female))
+var nombresFemeninos = function () { };
+//console.log(nombresFemeninos);
+//#8.-Retornar una array de strings (el email de los usarios de sexo masculino (Male))
+var mailsMasculinos = function () { };
+//console.log(mailsMasculinos);
+//#9.-Retornar un array de objetos que solo contengan los nombres, apellidos y ciudades de todos los usuarios
+var datosUsers = function () { };
+//console.log(datosUsers);
+//#10.-Retornar un array de objetos que solo contengan los nombres, apellidos y ciudades de todos los usuarios
+// masculinos mayores de 35 años
+var datosUsersMayoresMasculinos = function () { };
+//console.log(datosUsersMayoresMasculinos);
+//#11.-Retornar el promedio de edad de los usuarios
+var promedioEdad = function () { };
+//console.log(promedioEdad);  
+//#12.-Retornar el promedio de edad de los usuarios masculinos
+var promedioEdadMasculinos = function () { };
+//console.log(promedioEdadMasculinos());  
+//#13.-Retornar el promedio de edad de los usuarios egipcios (Egypt)
+var promedioEdadEgipcios = function () { };
+//console.log(promedioEdadEgipcios());  
+//# sourceMappingURL=ejercicios.js.map
---
+++ Clase09/ejercicios/ejercicios.ts
@@ -0,0 +1,55 @@
+
+/// <reference path="MOCK_DATA.ts" />
+
+let users = datos.MOCK_DATA;
+
+//#1.-Mostrar todos los datos de los usuarios por consola
+
+//#2.-Retornar todos los trabajos de los usuarios
+let trabajos = function(){};
+//console.log(trabajos);
+
+//#3.-Retornar todos los paises de los usuarios
+let paises = function(){};
+//console.log(paises);
+
+//#4.-Retornar un array de objetos de aquellos usuarios cuyo pais sea China
+let chinas = function(){};
+//console.log(chinas);
+
+//#5.-Retornar una array de objetos de todos los usuarios menores a 21 años
+let jovenes = function(){};
+//console.log(jovenes);
+
+//#6.-Retornar la cantidad de usuarios con sexo masculino (Male)
+let cantidadMasculinos = function() {};
+//console.log(cantidadMasculinos);
+
+//#7.-Retornar una array de strings (el nombre de los usarios de sexo femenino (Female))
+let nombresFemeninos = function() {};
+//console.log(nombresFemeninos);
+
+//#8.-Retornar una array de strings (el email de los usarios de sexo masculino (Male))
+let mailsMasculinos = function() {};
+//console.log(mailsMasculinos);
+
+//#9.-Retornar un array de objetos que solo contengan los nombres, apellidos y ciudades de todos los usuarios
+let datosUsers = function() {};
+//console.log(datosUsers);
+
+//#10.-Retornar un array de objetos que solo contengan los nombres, apellidos y ciudades de todos los usuarios
+// masculinos mayores de 35 años
+let datosUsersMayoresMasculinos = function(){};
+//console.log(datosUsersMayoresMasculinos);
+
+//#11.-Retornar el promedio de edad de los usuarios
+let promedioEdad = function() {};
+//console.log(promedioEdad);  
+
+//#12.-Retornar el promedio de edad de los usuarios masculinos
+let promedioEdadMasculinos = function() {};
+//console.log(promedioEdadMasculinos());  
+
+//#13.-Retornar el promedio de edad de los usuarios egipcios (Egypt)
+let promedioEdadEgipcios = function() {};
+//console.log(promedioEdadEgipcios());  
